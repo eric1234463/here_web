5c220ca336c04b105144865826a2b3d777bf5403
eric1234463 <eric1234463@gmail.com>
2017-11-14 18:15:50 +0800

    update editor

--- src/app/main/quick-panel/quick-panel.component.ts
+++ src/app/main/quick-panel/quick-panel.component.ts
@@ -1,29 +1,27 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
-    selector     : 'fuse-quick-panel',
-    templateUrl  : './quick-panel.component.html',
-    styleUrls    : ['./quick-panel.component.scss'],
+    selector: 'fuse-quick-panel',
+    templateUrl: './quick-panel.component.html',
+    styleUrls: ['./quick-panel.component.scss'],
     encapsulation: ViewEncapsulation.None
 })
-export class FuseQuickPanelComponent implements OnInit
-{
+export class FuseQuickPanelComponent implements OnInit {
     date: Date;
     settings: any;
-
-    constructor()
-    {
+    events: any;
+    notes: any;
+    constructor() {
         this.date = new Date();
         this.settings = {
             notify: true,
-            cloud : false,
-            retro : true
+            cloud: false,
+            retro: true
         };
 
     }
 
-    ngOnInit()
-    {
+    ngOnInit() {
 
     }
 
