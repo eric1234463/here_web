6ea32cc742ab3b361d650355a766365d28d81ddb
eric1234463 <eric1234463@gmail.com>
2018-03-04 23:25:47 +0800

    UPDATE qr code setting

--- src/app/main/content/qr/qr.component.ts
+++ src/app/main/content/qr/qr.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FuseTranslationLoaderService } from "../../../core/services/translation-loader.service";
 import { Observable } from "rxjs/Observable";
 import { Socket } from "ng-socket-io";
@@ -13,45 +13,48 @@ import { Doctor, Record } from "../../../services/constant";
     templateUrl: "./qr.component.html",
     styleUrls: ["./qr.component.scss"]
 })
-export class FuseSampleComponent {
+export class FuseSampleComponent implements OnInit {
     public connection: Boolean = false;
     public loadingIndicator = true;
     public records: Record[];
-    public uid: String;
+    public id: number;
     constructor(
         private translationLoader: FuseTranslationLoaderService,
         public socket: Socket,
         public http: HttpClient,
         public userService: UserService
     ) {
-        this.userService.getUser().then(user => {
-            this.uid = user.uid;
-            this.socket.connect();
-            this.socket.emit("subscribe", user.uid);
-            this.translationLoader.loadTranslations(english, turkish);
-            new Promise((resolve, reject) => {
-                this.socket.on("cancel doctor", function(data) {
-                    resolve();
-                });
-            }).then(() => {
-                this.connection = false;
+
+    }
+
+    async ngOnInit() {
+        const user = await this.userService.getUser();
+        this.id = user.id;
+        this.socket.connect();
+        this.socket.emit("subscribe", user.id);
+        this.translationLoader.loadTranslations(english, turkish);
+        new Promise((resolve, reject) => {
+            this.socket.on("cancel doctor", function(data) {
+                resolve();
             });
-            new Promise((resolve, reject) => {
-                this.socket.on("connect doctor", function(data) {
-                    const patient: String = data.patient;
-                    resolve(patient);
-                });
-            }).then(patient => {
-                this.http
-                    .get<Record[]>(
-                        `https://herefyp.herokuapp.com/api/record?userId=${patient}`
-                    )
-                    .subscribe(records => {
-                        this.connection = true;
-                        this.records = records;
-                        this.loadingIndicator = false;
-                    });
+        }).then(() => {
+            this.connection = false;
+        });
+        new Promise((resolve, reject) => {
+            this.socket.on("connect doctor", function(data) {
+                const patient: String = data.patient;
+                resolve(patient);
             });
+        }).then(patient => {
+            this.http
+                .get<Record[]>(
+                    `https://herefyp.herokuapp.com/api/record?userId=${patient}`
+                )
+                .subscribe(records => {
+                    this.connection = true;
+                    this.records = records;
+                    this.loadingIndicator = false;
+                });
         });
     }
 }
